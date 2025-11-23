3c5918b6bed5b853d20f6be5e6ea80e2b5b5ba61
Eli Peters <elip271828@gmail.com>
2025-11-23 18:04:02 -0500

    Fixed the level loading. They now only load the levels that exist.

--- frontend/src/Levels.jsx
+++ frontend/src/Levels.jsx
@@ -1,13 +1,14 @@
 import './Levels.css'
 import {useState} from "react"
+import {useParams} from "react-router-dom"
 
-function Levels({names, loggedIn}) {
+function Levels({names, loggedIn, setLoggedIn}) {
+    const {id} = useParams()
+
     const [lvlPg, setLvlPg] = useState(0)
 
-    const user = names.find(n => n.id === loggedIn)
+    const user = names.find(n => n.id === id)
     const levels = user ? user.lvl : []
-
-    console.log(levels)
 
     function getLevels() {
         const giveLevels =[]
@@ -21,23 +22,27 @@ function Levels({names, loggedIn}) {
             const completed = levels[lvlPgA + i] === true
             const lvlMes = completed ? "Completed" : "Not Completed"
 
-            giveLevelsA.push(
-                <div className={`level ${completed ? "" : "not"}`} key={"A" + lvlPgA + i}>
-                    <h1 className="lvl-title">Level {lvlPgA + i + 1}</h1>
-                    <h2 className="lvl-completed">{lvlMes}</h2>
-                </div>
-            )
+            if ((lvlPgA + i) < levels.length) {
+                giveLevelsA.push(
+                    <div className={`level ${completed ? "" : "not"}`} key={"A" + lvlPgA + i}>
+                        <h1 className="lvl-title">Level {lvlPgA + i + 1}</h1>
+                        <h2 className="lvl-completed">{lvlMes}</h2>
+                    </div>
+                )
+            }   
         }
         for (let i = 0; i < 3; i++) {
             const completed = levels[lvlPgB + i] === true
             const lvlMes = completed ? "Completed" : "Not Completed"
 
-            giveLevelsB.push(
-                <div className={`level ${completed ? "" : "not"}`} key={"B" + lvlPgB + i}>
-                    <h1 className="lvl-title">Level {lvlPgB + i + 1}</h1>
-                    <h2 className="lvl-completed">{lvlMes}</h2>
-                </div>
-            )
+            if ((lvlPgB + i) < levels.length) {
+                giveLevelsB.push(
+                    <div className={`level ${completed ? "" : "not"}`} key={"B" + lvlPgB + i}>
+                        <h1 className="lvl-title">Level {lvlPgB + i + 1}</h1>
+                        <h2 className="lvl-completed">{lvlMes}</h2>
+                    </div>
+                )
+            }
         }
 
         giveLevels.push(
