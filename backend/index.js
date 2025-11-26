40bca2568a5208d8efdc5f06cc528dbb96126c7f
Eli Peters <elip271828@gmail.com>
2025-11-26 11:45:52 -0500

    Backend deployment.

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 const corsOptions = {
     origin: [
-        'http://localhost:5173'
+        'https://framework-pink-nine.vercel.app'
     ],
     credentials: true,
 }
